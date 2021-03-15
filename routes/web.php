f0979dd8a7af41a31fe424610647ee4bfa4484e7
吴守雍 <872789604@qq.com>
2021-03-16 00:48:45 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PagesController@root')->name('root');  // 首页
 
-
 // 用户身份验证相关的路由
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
