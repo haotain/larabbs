b325b945cffd9f7f7a264795f74d8963a947f7c4
haotian <872789604@qq.com>
2021-03-18 00:41:19 +0800

    用户个人页面原型

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -24,8 +24,8 @@
               {{ Auth::user()->name }}
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="">个人中心</a>
-              <a class="dropdown-item" href="">编辑资料</a>
+              <a class="dropdown-item" href=" {{ route('users.show',Auth::user()->id) }} ">个人中心</a>
+              <a class="dropdown-item" href=" {{ route('users.edit',Auth::user()->id) }} ">编辑资料</a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" id="logout" href="#">
                 <form method="post" action="{{ route('logout') }}">
