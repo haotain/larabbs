a705669bd7698f9ac9ae33a997b3500fade43a32
haotian <872789604@qq.com>
2021-03-22 16:43:52 +0800

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+  @yield('styles')
 </head>
 
 <body>
@@ -29,6 +31,7 @@
 
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
+  @yield('scripts')
 </body>
 
 </html>
