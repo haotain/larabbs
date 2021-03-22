5fe9c69aa996e1e7a0bb7ce31aa74069a4fede32
haotian <872789604@qq.com>
2021-03-22 16:20:13 +0800

    用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
+}
+
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return Str::limit($excerpt, $length);
 }
