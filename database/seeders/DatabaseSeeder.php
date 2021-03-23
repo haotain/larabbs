c82e7591ea801de412a6e5dc5adf6233b981f4d5
haotian <872789604@qq.com>
2021-03-23 14:11:03 +0800

    回复数据

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+		$this->call(RepliesTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
+        $this->call(ReplicTableSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
