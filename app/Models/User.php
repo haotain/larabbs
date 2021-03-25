8e3499aea59ba4c70c18ac09d73ee02b531b4365
haotian <872789604@qq.com>
2021-03-25 12:41:37 +0800

    内容管理后台

--- app/Models/User.php
+++ app/Models/User.php
@@ -91,12 +91,13 @@ class User extends Authenticatable implements MustVerifyEmailContract
     public function setPasswordAttribute($value)
     {
         // 如果值的长度等于 60，即认为是已经做过加密的情况
-        if (strlen($value != 60)) {
+        if (strlen($value) != 60) {
             // 不等于 60，做密码加密处理
             $value = bcrypt($value);
         }
         $this->attributes['password'] = $value;
     }
+
 
     public function setAvatarAttribute($path)
     {
