50132d79920be11c3590df936979eada7fdfb064
haotian <872789604@qq.com>
2021-04-02 17:56:12 +0800

    图片验证码

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,8 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
         Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
         // 用户注册
         Route::post('users', 'UsersController@store')->name('users.store');
+        // 图片验证
+        Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
     });
 
     Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function() {
