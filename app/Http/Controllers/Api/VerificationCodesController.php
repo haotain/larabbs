c64514246c15410bfe132067ea6427585ab34ff1
haotian <872789604@qq.com>
2021-04-02 15:15:39 +0800

    发送短信验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -14,24 +14,35 @@ class VerificationCodesController extends Controller
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
         $phone = $request->phone;
+        var_dump(app()->environment('production'), app()->isLocal(), app()->environment('local'));die;
 
-        // 生成4位随机数，左侧补0
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        if (!app()->environment('production')) {
+            
+            $code = 1234;
 
-        try {
-            $result = $easySms->send($phone, [
-                'template' => config('easysms.gateways.aliyun.templates.register'),
-                'data' => [
-                    'code' => $code
-                ]
-            ]);
+        } else {
 
-        } catch (NoGatewayAvailableException $exception) {
-            $message = $exception->getException('aliyun')->getMessage();
-            abort(500, $message ?: '短信发送异常');
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+            try {
+                $result = $easySms->send($phone, [
+                    'template' => config('easysms.gateways.aliyun.templates.register'),
+                    'data' => [
+                        'code' => $code
+                    ]
+                ]);
+
+            } catch (NoGatewayAvailableException $exception) {
+                $message = $exception->getException('aliyun')->getMessage();
+                abort(500, $message ?: '短信发送异常');
+            }
         }
 
+
         $key = 'verificationCode_' . Str::random(15);
+
+        // 缓存验证码 5 分钟过期。
         $expiredAt = now()->addMinute(5);
 
         Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
@@ -40,6 +51,6 @@ class VerificationCodesController extends Controller
         return response()->json([
             'key' => $key,
             'expired_at' => $expiredAt->toDateTimeString()
-            ]);
+            ])->setStatusCode(201);
     }
 }
