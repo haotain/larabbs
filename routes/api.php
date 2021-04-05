c17772e7b861c163d5709af7b1e39f0543b61a2e
haotian <872789604@qq.com>
2021-04-05 21:09:11 +0800

    删除回复

--- routes/api.php
+++ routes/api.php
@@ -61,6 +61,10 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
             Route::post('images', 'ImagesController@store')->name('images.store');
             // 发布话题
             Route::resource('topics', 'TopicsController')->only(['store', 'update', 'destroy']);
+            // 发布回复
+            Route::post('topics/{topic}/replies', 'RepliesController@store')->name('topics.replies.store');
+            // 删除回复
+            Route::delete('topics/{topic}/replies/{reply}', 'RepliesController@destory')->name('topics.replies.destory');
         });
     });
 
